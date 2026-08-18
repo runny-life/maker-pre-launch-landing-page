728ddecdd165ce0435b5c83be2fdbf4801f06d41
runny-life(Nikita) <runny_life@icloud.com>
2026-08-18 19:11:35 +0300

    feat: add section header

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,14 @@
 import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
+import createSvgSpritePlugin from 'vite-plugin-svg-spriter';
+import path from 'path';
+
+const SRC_PATH = path.resolve(__dirname, 'src');
+const SVG_FOLDER_PATH = path.resolve(SRC_PATH, 'assets', 'icons');
 
 export default defineConfig({
   server: {
     open: true,
   },
-  plugins: [tailwindcss()],
+  plugins: [tailwindcss(), createSvgSpritePlugin({ svgFolder: SVG_FOLDER_PATH })],
 });
